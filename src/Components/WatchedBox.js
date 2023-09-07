a8098062793561f01a6d1e284b880cc614bd7fec
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-09-07 22:59:04 +0100

    Feat: Props Composition

--- src/Components/WatchedBox.js
+++ src/Components/WatchedBox.js
@@ -1,28 +1,26 @@
-import { useState } from "react";
-import { tempWatchedData } from "./Main";
-import { WatchedMovieSummary } from "./WatchedMovieSummary";
-import WatchedMovieList from "./WatchedMovieList";
+// import { useState } from "react";
+// import { tempWatchedData } from "./Main";
+// import { WatchedMovieSummary } from "./WatchedMovieSummary";
+// import WatchedMovieList from "./WatchedMovieList";
 
-export default function WatchedBox() {
-  const [watched, setWatched] = useState(tempWatchedData);
-  const [isOpen2, setIsOpen2] = useState(true);
+// export default function WatchedBox() {
+//   const [watched, setWatched] = useState(tempWatchedData);
+//   const [isOpen2, setIsOpen2] = useState(true);
 
-  return (
-    <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen2((open) => !open)}
-      >
-        {isOpen2 ? "–" : "+"}
-      </button>
-      {isOpen2 && (
-        <>
-          <WatchedMovieSummary watched={watched} />
-          <WatchedMovieList watched={watched} />
-        </>
-      )}
-    </div>
-  );
-}
-
-
+//   return (
+//     <div className="box">
+//       <button
+//         className="btn-toggle"
+//         onClick={() => setIsOpen2((open) => !open)}
+//       >
+//         {isOpen2 ? "–" : "+"}
+//       </button>
+//       {isOpen2 && (
+//         <>
+//           <WatchedMovieSummary watched={watched} />
+//           <WatchedMovieList watched={watched} />
+//         </>
+//       )}
+//     </div>
+//   );
+// }
