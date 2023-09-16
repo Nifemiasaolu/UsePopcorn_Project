d95ccafd2acbcfcad09ac5d3e2182fad12991973
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-09-16 23:47:54 +0100

    Summary on watched movies

--- src/Components/WatchedMovieSummary.js
+++ src/Components/WatchedMovieSummary.js
@@ -1,9 +1,9 @@
 import { average } from "./Main";
 
 export function WatchedMovieSummary({ watched }) {
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
+  const avgImdbRating = (average(watched.map((movie) => movie.imdbRating))).toFixed(1);
+  const avgUserRating = (average(watched.map((movie) => movie.userRating))).toFixed(1);
+  const avgRuntime = Math.round(average(watched.map((movie) => movie.runtime)))
 
   return (
     <div className="summary">
