c34d9b42da9c7e239ac201615ccebae4fb109064
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-09-18 23:12:04 +0100

    Movies Custom Hook

---
+++ src/Custom Hooks/useMovies.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+// import { KEY } from "react";
+
+const KEY = "3acf5bd0";
+
+export function useMovies(query) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  // Fetch Movie Data Effect by Search
+  useEffect(
+    function () {
+      // callback?.();
+      const controller = new AbortController();
+
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
+          );
+
+          if (!res.ok)
+            throw new Error("Something went wrong with fetching movies!");
+
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("Movie not found!");
+
+          setMovies(data.Search);
+        } catch (err) {
+          if (err.name !== "AbortError") {
+            setError(err.message);
+            // console.log(err.message);
+            setError("");
+          }
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
+      fetchMovies();
+
+      // CleanUp
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+
+  return { movies, isLoading, error };
+}
