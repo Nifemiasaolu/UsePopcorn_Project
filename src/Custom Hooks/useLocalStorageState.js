35b597e73eea58df347a22776edbe2532bcaa55a
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-09-18 23:11:51 +0100

    Local Storage Custom Hook

---
+++ src/Custom Hooks/useLocalStorageState.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorageState(initialState, key) {
+  const [value, setValue] = useState(function () {
+    const storage = localStorage.getItem(key);
+    return storage ? JSON.parse(storage) : initialState; 
+  });
+
+  // Store data in LocalStorage Effect
+  useEffect(
+    function () {
+      localStorage.setItem(key, JSON.stringify(value));
+    },
+    [value, key]
+  );
+
+  return [value, setValue];
+}
